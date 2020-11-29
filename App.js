b5ac6a525864567e7beb76b59e1c71c8cd86126e
disk0401 <mail@disk0401.net>
2020-11-30 00:03:10 +0900

    ニュース表示エリアをコンポーネント化

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import ListItem from './components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,48 +9,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row',
-  },
-  leftContiner: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray',
-  },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContiner}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ url: 'https://picsum.photos/id/10/200/200' }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            だるまさんがころんだだるまさんがころんだだるまさんがころんだだるまさんがころんだだるまさんがころんだだるまさんがころんだだるまさんがころんだ
-          </Text>
-          <Text style={styles.subText}>TestNews</Text>
-        </View>
-      </View>
+      <ListItem />
     </View>
   );
 }
