8478ca7c1e7cb161d91958b044a2024d7bd4548a
disk0401 <mail@disk0401.net>
2020-12-02 01:34:35 +0900

    AppNavigaterを利用して表示するように変更

---
+++ screens/HomeScreen.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import ListItem from '../components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default HomeScreen = () => {
+  // 初回ロード時は、空データをステートにセットする
+  const [articles, setArticles] = useState([]);
+  // ロード時に１時度だけ、記事データをセットする
+  useEffect(() => {
+    fetchAriticles();
+  }, []);
+
+  const fetchAriticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+      // console.log(response);
+    } catch {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+};
