012cdc99dcdfcd4c05727a8ddbe813d5b326a974
disk0401 <mail@disk0401.net>
2020-12-03 23:18:58 +0900

    BottomTabNavigatorを利用して、下側にタブを表示させる

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Article"
+        component={ArticleScreen}
+        options={{ headerShown: true }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Article"
-          component={ArticleScreen}
-          options={{ headerShown: true }}
-        />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
